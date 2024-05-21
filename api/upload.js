692716fdcface915cd80053fbb961be2cb1e5bc1
djelijah <ilijadjordjevic@hotmail.com>
2024-05-21 14:24:08 +0200

    Add env config

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,9 @@
 const AWS = require('aws-sdk');
 const formidable = require('formidable');
 const fs = require('fs');
+import { configDotenv } from 'dotenv';
+
+configDotenv
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
